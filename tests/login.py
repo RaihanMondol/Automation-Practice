7d536ff798a4b7d7905e54ea798612d959e76274
Raihan <raihanmondolbd@gmail.com>
2022-01-31 21:57:41 +0600

    create a simple login test

--- tests/login.py
+++ tests/login.py
@@ -1,10 +1,21 @@
 from selenium import webdriver
 import time
 
-driver = webdriver.Chrome(executable_path="F:\\Automation\\drivers\\chromedriver.exe")
-# driver = webdriver.Firefox(executable_path="F:\\Automation\\drivers\\geckodriver.exe")
+driver = webdriver.Chrome(executable_path="F:/Automation/drivers/chromedriver.exe")
+
 driver.implicitly_wait(10)
 driver.maximize_window()
-driver.get("https://www.youtube.com")
-driver.find_element_by_name("search_query").send_keys("Automation in python")
-driver.find_element_by_id("search-icon-legacy").click()
+
+driver.get("https://opensource-demo.orangehrmlive.com/")
+
+driver.find_element_by_id("txtUsername").send_keys("Admin")
+driver.find_element_by_id("txtPassword").send_keys("admin123")
+driver.find_element_by_id("btnLogin").click()
+
+driver.find_element_by_id("welcome").click()
+driver.find_element_by_link_text("Logout").click()
+
+time.sleep(4)
+driver.close()
+driver.quit()
+print("Task Completed!")
